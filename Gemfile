68be358910797671ad29c6f5187dfff933dd955c
Juan Pablo Ortiz <pablasso@gmail.com>
2013-07-04 13:38:59 -0500

    Removing old test.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem "railsless-deploy", "~> 1.1.2"
 gem "capistrano", "~> 2.15.5"
+gem "railsless-deploy", "~> 1.1.2"
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = 80;
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.use(express.static(__dirname + '/public'));
-
-app.get("/", function(req, res){
-	res.render("page");
+app.get('/', function(req, res){
+  res.send('Hello World!');
 });
 
-
-var io = require('socket.io').listen(app.listen(port));
-
-io.sockets.on('connection', function (socket) {
-	socket.emit('message', { message: 'welcome to the chat' });
-	socket.on('send', function (data) {
-		io.sockets.emit('message', data);
-	});
-});
-console.log("Listening on port " + port);
+app.listen(port);
+console.log('listening on port:' + port);
