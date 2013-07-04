f1e71363b869ccc74bec4e6a1ab990e73c2b65a1
Juan Pablo Ortiz <pablasso@gmail.com>
2013-07-04 10:12:52 -0500

    Current host for the chat.

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var messages = [];
-	var socket = io.connect('http://127.0.0.1:80');
+	var socket = io.connect(window.location.host + ':80');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
