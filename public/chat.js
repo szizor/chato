4020b18438969c13c82a748e5110b54db9bc8773
Juan Pablo Ortiz <pablasso@gmail.com>
2013-07-04 10:05:55 -0500

    Port 80 on socket.io

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var messages = [];
-	var socket = io.connect('http://localhost:3700');
+	var socket = io.connect('http://127.0.0.1:80');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
