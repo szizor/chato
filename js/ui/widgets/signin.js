559db543e68019fe83c0b775f1624d3fbdd1e56e
Noel Delgado <auhcsei@gmail.com>
2013-07-05 14:03:23 -0500

    login, register validation

--- js/ui/widgets/signin.js
+++ js/ui/widgets/signin.js
@@ -3,20 +3,20 @@ Class(Chato.UI, 'SignIn').inherits(Chato.UI.Widget)({
     html : '<div class="sign-in-screen screen">\
               <div class="container">\
                 <header class="flex">\
-                  <a href="#" class="back btn auto">Welcome</a>\
+                  <a href="#" class="back prev btn auto">Welcome</a>\
                   <h3 class="title auto">Sing In</h3>\
-                  <a href="#" class="done btn btn-success auto">Sing In</a>\
+                  <a href="#" class="done next btn btn-success auto">Sing In</a>\
                 </header>\
                 <div class="screen-body">\
                   <form action="">\
-                    <label for="">\
+                    <div class="control-group">\
                       <span class="fui-user"></span>\
-                      <input type="text" value="" placeholder="Username" class="span3">\
-                    </label>\
-                    <label for="">\
+                      <input type="email" value="" placeholder="Username" class="span3 username">\
+                    </div>\
+                    <div class="control-group">\
                       <span class="fui-lock"></span>\
-                      <input type="text" value="" placeholder="Password" class="span3">\
-                    </label>\
+                      <input type="password" value="" placeholder="Password" class="span3 password">\
+                    </div>\
                   </form>\
                 </div>\
               </div>\
@@ -29,6 +29,10 @@ Class(Chato.UI, 'SignIn').inherits(Chato.UI.Widget)({
             Chato.UI.Widget.prototype.init.apply(this, [args]);
 
             this.back = this.element.find('.back');
+            this.done = this.element.find('.done');
+            this.username = this.element.find('.username');
+            this.password = this.element.find('.password');
+
             this.bindEvents();
         },
 
@@ -38,6 +42,33 @@ Class(Chato.UI, 'SignIn').inherits(Chato.UI.Widget)({
             this.back.bind('click', function (ev) {
                 _this.back.parents('.screen').removeClass('show');
             });
+
+            this.done.bind('click', function (ev) {
+                _this.validateInputs();
+            });
+
+        },
+
+        validateInputs : function validateInputs () {
+            var username = this.username.val(),
+                password = this.password.val(),
+                pass = true;
+
+            if ( !username || !password ) pass = false;
+
+            ( !username )
+                ? this.username.parent().addClass('error')
+                : this.username.parent().removeClass('error');
+
+            ( !password )
+                ? this.password.parent().addClass('error')
+                : this.password.parent().removeClass('error');
+
+            if ( pass ) {
+                console.log("send form");
+            }
+
+            return this;
         },
 
         destroy : function destroy () {
