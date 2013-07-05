bdb50099b28961f2409ad822dad607b24ecbc717
azendal <trasvina@gmail.com>
2013-07-05 10:31:08 -0500

    listen for socket

--- server.js
+++ server.js
@@ -10,5 +10,15 @@ app.get('/', function(req, res){
   res.send('lets play');
 });
 
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
 app.listen(port);
 console.log('listening on port:' + port);
