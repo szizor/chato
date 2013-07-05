8bf650fd2b39ce95c0a8e297d6630a68253959b2
Alejandro <alejandro@freshout.us>
2013-07-05 11:05:41 -0500

    add user class

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ var app = express();
 var port = 5000;
 var users = {};
 
+var getuid = (function generateUid() {
+            var getUid = function(length){
+                var i, uid, min, max;
+
+                length = length || 32;
+                uid = '';
+                min = 0;
+                max = 14;
+                for(i = 0; i < length; i++){
+                    uid += getUid.codes[ Math.floor(Math.random() * (max - min + 1)) + min ];
+                }
+                return uid;
+            };
+
+            getUid.codes = [0, 1, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+            return getUid;
+        }());
+
 BASE_PATH = process.cwd();
 
 require(BASE_PATH + '/vendor/neon/neon.js');
 require(BASE_PATH + '/vendor/NodeSupport.js');
 require(BASE_PATH + '/lib/App.js');
 require(BASE_PATH + '/lib/Player.js');
+require(BASE_PATH + '/lib/User.js');
 require(BASE_PATH + '/lib/Channel.js');
 
 app.get('/', function(req, res){
