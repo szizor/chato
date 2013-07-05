faafcb60e91a8843831a05723ce1953b5be4aa58
azendal <trasvina@gmail.com>
2013-07-05 13:32:56 -0500

    channels are ready

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var messages = [];
-	var socket = io.connect('http://localhost:8080');
+	socket = io.connect('http://localhost:8080');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
