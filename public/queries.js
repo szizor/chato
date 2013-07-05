e8388fff9cd080c762e214f543362124577721b0
Alejandro <alejandro@freshout.us>
2013-07-05 10:13:51 -0500

    add proto

---
+++ public/queries.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+  // var sequelize = new Sequelize('chat', 'root', null);
+  // var Room = sequelize.define('rooms', {
+  //   title:       Sequelize.STRING
+  // });
+  // var Conversation = sequelize.define('conversations', {
+  //   title:       Sequelize.STRING,
+  //   rooms_id:    Sequelize.INTEGER
+  // });
+  // Room.hasMany(Conversation);
+  // var task = Task.build({title: 'very important task'})
+  // task.title // ==> 'very important task'
+  // // persist an instance
+  // task.save()
+  //   .error(function(err) {
+  //     // error callback
+  //   })
+  //   .success(function() {
+  //     console.log("query saved!")
+  //   });
+
+  // // query persistence for instances
+  // var tasks = Task.all({ where: ['dealdine < ?', new Date()] })
+  //   .error(function(err) {
+  //     // error callback
+  //   })
+  //   .success(function() {
+  //     console.log("queried")
+  //   });
+}
